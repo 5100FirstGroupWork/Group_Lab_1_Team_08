b632c449ddb0ed6656b0b34094ee410863c83928
ZitiantaoLin <lin.ziti@northeastern.edu>
2025-03-03 21:34:18 -0500

    first commit

---
+++ TeamGreeting/src/model/Main.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import model.Member1;
+
+/**
+ *
+ * @author 林子天涛
+ */
+public class Main {
+    public static void main(String[] args) {
+        Member1.main(args);
+    }
+}
