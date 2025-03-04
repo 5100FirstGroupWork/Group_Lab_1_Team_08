f6dc5622ab19fc96b39419abe4f0f41ccc401f86
Anqi Bao <anqi@Anqis-MacBook-Air.local>
2025-03-03 22:25:23 -0500

    push member 2 code

---
+++ Lab_6/src/model/GitHubPractice.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import model.GitHubPractice.Member2;
+
+/**
+ *
+ * @author anqi
+ */
+public class GitHubPractice {
+
+    public class Member2 {
+
+        public static void main(String[] args) {
+            System.out.println("Hello from Member 2!");
+        }
+    }
+}
