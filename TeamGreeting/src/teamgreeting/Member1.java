f1d9b5cfcf1d983956623fce5810fe12cde1c06c
ZitiantaoLin <lin.ziti@northeastern.edu>
2025-03-03 20:32:29 -0500

    member1

--- TeamGreeting/src/teamgreeting/Member1.java
+++ TeamGreeting/src/teamgreeting/Member1.java
@@ -10,6 +10,6 @@ package teamgreeting;
  */
 public class Member1 {
     public static void main(String[] args) {
-        System.out.println("Hello from Member 1!");
+        System.out.println("Hello from Zitiantao Lin!");
     }
 }
